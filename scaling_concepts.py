1d67708e035c2654ac0e908b40c874b87a351785
Vaibhav Sharma <hihello44694@gmail.com>
2026-08-08 19:46:26 +0530

    Updated scaling_concepts.py

--- scaling_concepts.py
+++ scaling_concepts.py
@@ -2,12 +2,35 @@ from sklearn.preprocessing import StandardScaler
 
 import pandas as pd
 
-data = pd.DataFrame({
+fitTransform_data = pd.DataFrame({
     "Age": [20,30,40,50,60]
 })
 
+# Using Standard Scalar
+
 scaler = StandardScaler()
 
-scaled_data = scaler.fit_transform(data)
+# using fit_transform on data to learn the mean and standard deviation from the training data.
 
-print(scaled_data)
+trained_data_using_fit_transform = scaler.fit_transform(fitTransform_data)
+
+print(trained_data_using_fit_transform)
+
+X_train = [
+    [20, 20000],
+    [30, 30000],
+    [40, 40000],
+    [50, 50000]
+]
+
+X_test = [
+    [60, 60000]
+]
+
+# using fit_transform on data to learn the mean and standard deviation from the training data.
+X_train_scaled = scaler.fit_transform(X_train)
+
+# using transform to only scale
+X_test_scaled = scaler.transform(X_test)
+
+print(X_test_scaled)
